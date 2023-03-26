9034f39d116c133e3a98a209425787a391e03b23
Yaqi Gao <yukigao813@gmail.com>
2023-03-25 22:45:01 -0400

    Traffic logic v1

--- traffic_logic/traffic_logic.ino
+++ traffic_logic/traffic_logic.ino
@@ -1,16 +1,5 @@
 int light_duration = 5;
 int max_duration = 15;
-
-// struct lane{
-//   int rate;
-//   int cars;
-// };
-
-// lane* north;
-// lane* south;
-// lane* east;
-// lane* west;
-
 /*
   N, S, E, W
 */
@@ -19,14 +8,24 @@ int density_eval[4];
 int no_density_eval[4];
 int rate[] = {1,0,0,0}; 
 
-int ew_total;
-int ns_total;
 
-bool ew_light;
-bool ns_light;
+bool ew_light = false;
+bool ns_light = false;
 
 int clk=0;
-int max_clk=60;
+int max_clk=30;
+
+int density_incoming = 0;
+int no_density_incoming = 0;
+
+int sum(int a[]){
+  int s = 0;
+  int len = sizeof(a) / sizeof(a[0]);
+  for (int i = 0; i<len; i++){
+    s+=a[i];
+  }
+  return s;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -34,15 +33,23 @@ void setup() {
   lane_cars[1] = 5;
   lane_cars[2] = 10;
   lane_cars[3] = 10;
+  int initial = sum(lane_cars);
   //lane_cars = {20,5,10,10};
-  density();
+  int density_wait_time = density();
+
+
   lane_cars[0] = 20;
   lane_cars[1] = 5;
   lane_cars[2] = 10;
   lane_cars[3] = 10;
-  w_o_density();
+  int no_density_wait_time = w_o_density();
 
-  evaluation();
+  evaluation(initial,density_wait_time,no_density_wait_time);
+
+  //Serial.println(density_incoming);
+  //Serial.println(no_density_incoming);
+  //Serial.println(density_wait_time/(initial+density_incoming));
+  //Serial.println(no_density_wait_time/(initial+no_density_incoming));
 
   //delay(10000); //delay 10seconds between each case
 }
@@ -53,9 +60,9 @@ void loop() {
 
 }
 
-void density(){
+int density(){
   clk = 0;
-
+  int wait_time = 0;
   while(clk<max_clk){
     int maxIndex = 0;
     int maxValue = lane_cars[maxIndex];
@@ -93,22 +100,29 @@ void density(){
       }
     }
 
-    while(duration >0){
+    while(clk<max_clk && duration >0){
       if (ew_light){
           lane_cars[0] += rate[0];
           lane_cars[1] += rate[1];
           lane_cars[2] = lane_cars[2]==0? 0: lane_cars[2]-1;
           lane_cars[3] = lane_cars[3]==0? 0: lane_cars[3]-1;
+
+          wait_time +=sum(lane_cars);
+          density_incoming += rate[0]+rate[1];
         }
       else if (ns_light){
           lane_cars[0] = lane_cars[0]==0? 0: lane_cars[0]-1;
           lane_cars[1] = lane_cars[1]==0? 0: lane_cars[1]-1;
           lane_cars[2] += rate[2];
           lane_cars[3] += rate[3];
+
+          wait_time +=sum(lane_cars);
+          density_incoming += rate[2]+rate[3];
       }
       duration -=1;
       Serial.println(clk);
       clk++;
+      
       print_status();
       delay(100);
 
@@ -117,23 +131,31 @@ void density(){
   
   }
   memcpy(density_eval, lane_cars, sizeof(lane_cars));
+  return wait_time;
 }
 
 
-void w_o_density(){
+int w_o_density(){
   clk=0;
+  int wait_time = 0;
   while (clk < max_clk){
     if (clk % 10 <5){
       lane_cars[0] = lane_cars[0]==0? 0: lane_cars[0]-1;
       lane_cars[1] = lane_cars[1]==0? 0: lane_cars[1]-1;
       lane_cars[2]+=rate[2];
       lane_cars[3]+=rate[3];
+
+      wait_time +=sum(lane_cars);
+      no_density_incoming += rate[2]+rate[3];
     }
     else{
       lane_cars[0]+=rate[0];
       lane_cars[1]+=rate[1];
       lane_cars[2] = lane_cars[2]==0? 0: lane_cars[2]-1;
       lane_cars[3] = lane_cars[3]==0? 0: lane_cars[3]-1;
+
+      wait_time +=sum(lane_cars);
+      no_density_incoming += rate[0]+rate[1];
     }
     Serial.println(clk);
     print_status();
@@ -146,19 +168,21 @@ void w_o_density(){
     }
   }
   memcpy(no_density_eval, lane_cars, sizeof(lane_cars));
+  return wait_time;
 }
 
-void evaluation(){
-  int incoming = max_clk;
-  int density_sum, no_density_sum =0;
-  for (int i=0; i<4;i++){
-    no_density_sum += no_density_eval[i];
-    density_sum += density_eval[i];  
-  }
-  Serial.print("with optimization: ");
-  Serial.println( density_sum / incoming);  
-  Serial.print("without optimization: ");
-  Serial.println(no_density_sum / incoming);
+void evaluation(int initial, int density_wait_time, int no_density_wait_time){
+  // double incoming = max_clk;
+  // double density_sum, no_density_sum =0;
+  // for (int i=0; i<4;i++){
+  //   no_density_sum += no_density_eval[i];
+  //   density_sum += density_eval[i];  
+  // }
+
+  Serial.print("average wait time with optimization: ");
+  Serial.println(density_wait_time/(initial+density_incoming));
+  Serial.print("average wait time without optimization: ");
+  Serial.println(no_density_wait_time/(initial+no_density_incoming));
 }
 
 void print_status(){
